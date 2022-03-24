179e15bfc278bc07aa9afeae30a3cfeb94baf5c1
Monika Rafalska <82111615+MonikaRafalska@users.noreply.github.com>
2022-03-24 22:10:58 +0100

    fix

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,11 +2,9 @@ import React from "react";
 import Contacts from "../Contacts/Contacts";
 const ContactList = ({ contacts, onDeleteContact }) => {
   return (
-    <>
       <ul>
         <Contacts contacts={contacts} onDeleteContact={onDeleteContact} />
       </ul>
-    </>
   );
 };
 export default ContactList;
--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -2,22 +2,20 @@ import React from "react";
 import styles from "./Contacts.module.css";
 const Contacts = ({ contacts, onDeleteContact }) => {
   return (
-    <>
+    <ul className={styles.list}>
       <h2 className={styles.title}>Contacts</h2>
-      <ul className={styles.list}>
-        {contacts.map(({ id, name, number }) => (
-          <li key={id} className={styles.item}>
-            {name}: {number}
-            <button
-              className={styles.button}
-              type="button"
-              onClick={() => onDeleteContact(id)}>
-              Delete
-            </button>
-          </li>
-        ))}
-      </ul>
-    </>
+      {contacts.map(({ id, name, number }) => (
+        <li key={id} className={styles.item}>
+          {name}: {number}
+          <button
+            className={styles.button}
+            type="button"
+            onClick={() => onDeleteContact(id)}>
+            Delete
+          </button>
+        </li>
+      ))}
+    </ul>
   );
 };
 export default Contacts;
